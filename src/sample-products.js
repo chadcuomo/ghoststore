292459792bdb703f717c5f8f674af88d1b5381b1
Chad <chad.cuomo@yahoo.com>
2020-07-09 17:16:02 -0400

    added product images, added sample products file with an object of products

---
+++ src/sample-products.js
@@ -0,0 +1,74 @@
+const products = {
+  pw1: {
+    name: "Legend GrapeFruit",
+    image: "/images/pw-gf.png",
+    desc:
+      "Grapefruit perfection...from the textured grapefruit skin label to the classic true to fruit flavor, this limited edition GHOST Legend® looks, feels and tastes just like it was plucked from the Stafford Brother's tree.",
+    price: 4499,
+    status: "available"
+  },
+  pw2: {
+    name: "Legend",
+    image: "/images/pw-br.png",
+    desc:
+      "GHOST LEGEND® pre workout combines a stand out energy and focus formula with premium pump ingredients to get you feeling like a legend whenever you need it most.",
+    price: 4499,
+    status: "available"
+  },
+  pw3: {
+    name: "LEGEND X BUBBLICIOUS",
+    image: "/images/pw-bubble.png",
+    desc:
+      "We teamed up with the best (again) to deliver an authentic AF bubblegum pre-workout EXPERIENCE!",
+    price: 4499,
+    status: "available"
+  },
+  pw4: {
+    name: "LEGEND X SOUR PATCH KIDS",
+    image: "/images/pw-spk.png",
+    desc:
+      "You asked, we delivered. AUTHENTIC SOUR PATCH KIDS® flavor now in GHOST Legend®.",
+    price: 4499,
+    status: "available"
+  },
+  pw5: {
+    name: "LEGEND X SONIC",
+    image: "/images/pw-sonic.png",
+    desc:
+      "After a year of trial and error with the SONIC® team (and A TON of “R&D” trips to America’s Favorite Drive-In) we are so stoked to drop the best, and only official SONIC® Cherry Limeade.",
+    price: 4499,
+    status: "available"
+  },
+  pw6: {
+    name: "LEGEND X WARHEADS",
+    image: "/images/pw-wh.png",
+    desc:
+      "We partnered with WARHEADS® to deliver the first ever AUTHENTIC AF pre workout flavor collab in the game!",
+    price: 4499,
+    status: "available"
+  },
+  pw7: {
+    name: "LEGEND X WELCH'S",
+    image: "/images/pw-wgj.png",
+    desc: "Sure there's plenty of grape flavors on the market, but (in our opinion) nothing comes close to the EPIC grape flavor we all grew up on.",
+    price: 4499,
+    status: "unavailable"
+  },
+  pw8: {
+    name: "GAMER",
+    image: "/images/pw-ggp.png",
+    desc: "GHOST® GAMER delivers an EPIC combo of brain-boosting nootropics and natural energy to ensure you're dialed for an afternoon on the sticks or at the office.",
+    price: 3999,
+    status: "available"
+  },
+  pw9: {
+    name: "GAMER X WARHEADS",
+    image: "/images/pw-ggwh.png",
+    desc: "GHOST® GAMER delivers an EPIC combo of brain-boosting nootropics and natural energy to ensure you're dialed for an afternoon on the sticks or at the office.",
+    price: 3999,
+    status: "available"
+  }
+};
+
+export default products;
+  
