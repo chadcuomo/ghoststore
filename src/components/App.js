454fb42601a4cf4847d8307d4ab930489d29bc59
Chad <chad.cuomo@yahoo.com>
2020-07-10 18:06:56 -0400

    Modify hero image, + header text

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
         <div>
           <img src="/images/Ghost-Hero-image.jpg" alt="HERO" className="hero" />
         </div>
+        <div>
+          <h2 className="shopheader">Shop Supplements</h2>
+        </div>
       </div>
     );
   }
