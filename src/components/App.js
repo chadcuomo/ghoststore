5ab7e7646d4db3c3a30f2a62b8c38aa0cfc2677b
Chad <chad.cuomo@yahoo.com>
2020-07-11 13:59:05 -0400

    Added product component, started styling on styling for product component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Nav from './Nav';
+import sampleProducts from '../sample-products';
+import SmallProduct from'./SmallProduct';
 
 
 
 class App extends React.Component {
+  state = {
+    products: {}
+  };
+
+  loadSampleProducts = () => {
+    this.setState({ products: sampleProducts });
+  };
+
+  componentDidMount() {
+    this.loadSampleProducts();
+  }
+
   render() {
     return (
       <div className="main-container">
@@ -11,8 +25,19 @@ class App extends React.Component {
         <div>
           <img src="/images/Ghost-Hero-image.jpg" alt="HERO" className="hero" />
         </div>
-        <div>
-          <h2 className="shopheader">Shop Supplements</h2>
+        <div className="bottom-container">
+          <div className="header-container">
+            <h2 className="shopheader">Shop Supplements</h2>
+          </div>
+          <div className="products-container">
+            {Object.keys(this.state.products).map(key =>
+              <SmallProduct
+              key={key}
+              index={key}
+              details={this.state.products[key]}
+              />
+              )}
+          </div>
         </div>
       </div>
     );
---
+++ src/components/SmallProduct.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SmallProduct extends React.Component {
+  render() {
+    const { image, name, price, status } = this.props.details;
+    return (
+      <div className="small-product-outer">
+        <div className="small-product-inner">
+          <div className="small-img-container">
+            <img src={image} alt={name} className="small-prod-img" />
+          </div>
+          <div>
+            <hr />
+          </div>
+          <div className="product-info">
+            <div className="small-prod-title">
+              <h3>{name}</h3>
+            </div>
+            <div className="sub-info">
+              <p className="small-prod-subtitle">Pre-Workout</p>
+              <p className="small-price">{price}</p>
+            </div>
+            <div className="small-btn-container">
+              <button className="add-cart">Add To Cart</button>
+              <button className="details">Details</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SmallProduct;
